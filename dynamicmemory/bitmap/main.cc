1beb7231a457b41c14f08e37eb1726e27ceaccc7
Monami1989 <monami@DESKTOP-R6JOLLM.localdomain>
2018-11-09 10:39:48 -0500

    Adding final changes - Bitmap

--- dynamicmemory/bitmap/main.cc
+++ dynamicmemory/bitmap/main.cc
@@ -11,7 +11,7 @@ using namespace std;
 
 int main(){
   
-  Bitmap b(30,20); // 30 pixels across, 20 pixels high, each pixel RGBA
+  Bitmap b(100,100); // 30 pixels across, 20 pixels high, each pixel RGBA
 	           // top left pixel = (0,0)
   Color RED(255,0,0,255); // all red, no green, no blue (fully opaque)
   Color BLUE(0,0,255,255);
@@ -25,8 +25,8 @@ int main(){
   b.horizLine(0,20,19, GREEN); // from x=0 to x=20 at y=19 
   b.vertLine(5,0,19, GREEN); // from y = 0 to y=19 at x = 5 
   b.fillRect(10,10,4,3,BLACK); // x = 10, y =10 w=4, h=3 
-  /* b.drawRect(10,10, 4, 3,WHITE); // x = 10, y =10 w=4, h=3
-     b.ellipse(15,0, 8, 5, RED);    // ellipse centered at (15,0) w= 8, h=5*/
+  b.drawRect(10,10, 4, 3,WHITE); // x = 10, y =10 w=4, h=3
+  b.ellipse(50,50, 10, 14, RED);    // ellipse centered at (15,0) w= 8, h=5
   cout << b; 
   
 }
