5c092a8962bf7ac9048ca60f6b3ee8773daf7f6d
X Guo <3428041+xlyytcy@users.noreply.github.com>
2018-11-23 16:52:20 -0500

    Finished templated list.

--- templates/templatelist/GrowArray.hh
+++ templates/templatelist/GrowArray.hh
@@ -1,51 +1,81 @@
 #pragma once
 
 #include <iostream>
+#include <utility>
 
-template<typename T>
+template<class T>
 class GrowArray {
-private:
-	int used;
-	int capacity;
-	T* data;
-	void checkGrow() {
-		if (  )
-			return; // don't grow unless you have to
-		T* old = data;
 
-		//TODO: every time the list grows, double it!!!
-		data = new T[used+1]; // calls T::T()
-		for (int i = 0; i < used; i++)
-			data[i] = old[i];
-		used++;
-	}
+    int used; // Slots actually being used
+    int capacity; // The total capacity of the list
+    T *data;
+
+    void checkGrow() {
+        if (used == 0) {
+            capacity++;
+            data = new T[capacity * sizeof(T)];
+        }
+
+        if (used <= capacity) {
+            used++;
+            return; // don't grow unless you have to
+        }
+
+        T *old = data;
+        capacity *= 2;
+
+        data = new T[capacity * sizeof(T)];
+        for (int i = 0; i < used; i++)
+            data[i] = old[i];
+        // memcpy(data, old, capacity);
+        used++;
+        delete[] old;
+    }
+
 public:
-	GrowArray() : used(0), capacity(0), data(nullptr) {}
-	GrowArray(int  initialCapacity)
-		: used(0), capacity(initialCapacity), data( ) //TODO: fix! {
-			}
-	
-	//TODO: add destructor, copy constructor, operator =
-	void addEnd(const T& v) {
-		checkGrow();
-		data[length-1] = v;
-	}
+    GrowArray() : used(0), capacity(0), data(nullptr) {}
 
-	T removeEnd() {
-    length--;
-		return data[length];
-	}
-	T operator [](int i) const {
-		return data[i];
-	}
+    GrowArray(int initialCapacity) : used(0), capacity(initialCapacity), data(new T[capacity * sizeof(T)] ) {
+    }
 
-	T& operator [](int i) {
-		return data[i];
-	}
+    ~GrowArray() { delete[] data; }
 
-	friend std::ostream& operator <<(std::ostream& s, const GrowArray<T>& list) {
-    for (int i = 0; i < list.length; i++)
-			s << list[i] << ' ';
-		return s;
-	}
+    GrowArray(const GrowArray& orig) : capacity(orig.capacity), used(orig.used), data(new T[capacity * sizeof(T)]) {
+        for (int i = 0; i < used; i++)
+            data[i] = orig.data[i];
+    }
+
+    GrowArray& operator =(const GrowArray& orig) {
+        GrowArray temp = orig;		// same as GrowArray temp(orig);
+        used = temp.used;
+        capacity = temp.capacity;
+        data = temp.data;
+        temp.data = nullptr;
+        return *this;
+    }
+
+    void addEnd(const T &v) {
+        checkGrow();
+        data[used - 1] = v;
+    }
+
+    T removeEnd() {
+        used--;
+        return data[used];
+    }
+
+    T operator[](int i) const {
+        return data[i];
+    }
+
+    T &operator[](int i) {
+        return data[i];
+    }
+
+    friend std::ostream &operator<<(std::ostream &s, const GrowArray<T> &list) {
+        for (auto i = 0; i < list.used; i++)
+            s << list[i] << ' ';
+        return s;
+    }
+
 };
--- templates/templatelist/TestGrowArray.cc
+++ templates/templatelist/TestGrowArray.cc
@@ -1,30 +1,37 @@
 #include "GrowArray.hh"
 #include "string"
+
 using namespace std;
 
 class Elephant {
 private:
-	string name;
+    string name;
 public:
-	Elephant(const string& name) : name(name) {}
-	Elephant() : name() {}
-	friend ostream& operator <<(ostream& s, const Elephant& e) {
-		return s << e.name;
-	}
+    Elephant(const string &name) : name(name) {}
+
+    Elephant() : name() {}
+
+    friend ostream &operator<<(ostream &s, const Elephant &e) {
+        return s << e.name;
+    }
 };
 
 int main() {
-	GrowArray<int> a;
-	//TODO:	GrowArray b(1000);
-	for (int i = 0; i < 10; i++)
-		a.addEnd(i);
+    GrowArray<int> a;
+    GrowArray<int> b(1000);
+    for (int i = 0; i < 1000; i++)
+        a.addEnd(i);
 
-	a.removeEnd();
-	cout << a;
+    a.removeEnd();
+    cout << a;
 
-	GrowArray<Elephant> e;
-	e.addEnd(Elephant("Alice"));
-  e.addEnd(Elephant("Betty"));
-	cout << e << '\n';
+//    Test Cpy Cnstr
+//    b = a;
+//    cout << b;
+
+    GrowArray<Elephant> e;
+    e.addEnd(Elephant("Alice"));
+    e.addEnd(Elephant("Betty"));
+    cout << e << '\n';
 }
 	
