203b22fecf7ac1b803a08ddcc6e7f51d88ec16d0
Monami1989 <monami@DESKTOP-R6JOLLM.localdomain>
2018-11-25 16:51:06 -0500

    Fraction segment program

--- Fraction/main.cc
+++ Fraction/main.cc
@@ -1,10 +1,17 @@
-#include "Fraction.hh"
-
+/*
+Author: Monami Mukhopadhyay & Xingyuan Guo
+Program: Fraction
+*/
+#include<iostream>
+#include "Fraction.h"
+#include "Fraction.cc"
 using namespace std;
+
 int main() {
 	Fraction a(1,2);
 	Fraction b(1,3);
 	Fraction c = a + b;
-	Fraction c = a - b;
 	cout << c << '\n';
+	Fraction d = a - b;
+	cout << d << '\n';
 }
