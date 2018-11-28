3c50a554e819f7b314c86e9cba8a30893af7c698
X Guo <3428041+xlyytcy@users.noreply.github.com>
2018-11-27 22:54:28 -0500

    Told gitignore to ignore all test svg.

--- regex/regex_svg.cpp
+++ regex/regex_svg.cpp
@@ -7,8 +7,12 @@ void remove_tspan(){
 
 }
 
+// regex_match - match regex to an entire char sequence
+// regex_search - match regex to any part of a char sequence
+// regex_replace - replace occurrences of a regex with formatted replacement text
+// regex_iterator - iterates thru all regex matches within a char sequence
 void round_off(){
-
+    //regex floating_regex("\\-?\\d+\\.\\d+"");
 }
 
 
